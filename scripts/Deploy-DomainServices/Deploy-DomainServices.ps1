c82703cf777c9d9c5a563b656d19738d8bd6b2a1
Niels Kok <>
2025-08-27 11:27:20 +0200

    Updated it again

---
+++ scripts/Deploy-DomainServices/Deploy-DomainServices.ps1
@@ -0,0 +1,39 @@
+Configuration Deploy-DomainServices
+{
+    Param
+    (
+        [Parameter(Mandatory)]
+        [String] $domainFQDN,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential] $adminCredential
+    )
+
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName 'ActiveDirectoryDsc'
+    Import-DscResource -ModuleName 'ComputerManagementDsc'
+    Import-DscResource -ModuleName 'NetworkingDsc'
+
+    node 'localhost'
+    {
+        WindowsFeature 'ADDS'
+        {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature 'RSAT'
+        {
+            Name   = 'RSAT-AD-PowerShell'
+            Ensure = 'Present'
+        }
+
+        ADDomain 'createforest'
+        {
+            DomainName                    = $domainFQDN
+            Credential                    = $adminCredential
+            SafemodeAdministratorPassword = $adminCredential
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
